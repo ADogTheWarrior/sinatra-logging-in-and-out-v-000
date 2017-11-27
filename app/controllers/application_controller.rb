f6d6df071ef9853aa86a652feae794f46bbf944c
Adam Fudge <coolfudge@gmail.com>
2017-11-27 20:30:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     if !!(@user = User.find_by(username: params[:username], password: params[:password]))
+      puts @user
       session[:user_id] = @user.id
       redirect to '/account'
     else
